75aa4a0c404a45a2cc615ec4891c8d8da9c833b7
song-chaeyoung <153435389+song-chaeyoung@users.noreply.github.com>
2025-02-02 18:35:56 +0900

    Edit schedule css

--- src/components/schedule.tsx
+++ src/components/schedule.tsx
@@ -2,19 +2,33 @@ import React from "react";
 import style from "../styles/schedule.module.css";
 
 const Schedule = () => {
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
     <section className={style.container}>
       <div className={style.mainBox}>
-        <p>일정 추가하기</p>
-        <form>
+        <p className={style.title}>일정 추가하기</p>
+        <form className={style.form} onSubmit={submitHandler}>
           <div className={style.input_title}>
             <label htmlFor="input_title">일정 제목</label>
             <input type="text" id="input_title" />
           </div>
           <div>
-            <label htmlFor=""></label>
-            <input type="text" />
+            <label htmlFor="input_date">일정 날짜</label>
+            <input type="text" id="input_date" />
           </div>
+          <div>
+            <label htmlFor="input_time">일정 시간</label>
+            <input type="text" id="input_time" />
+          </div>
+          <div>
+            <label htmlFor="input_content">일정 내용</label>
+            <textarea name="input_content" id="input_content"></textarea>
+          </div>
+          <button className={style.submit_btn}>일정 추가</button>
         </form>
       </div>
     </section>
